9addc100003ddaeed7776c98fe490920f1380418
Bruno Henrique Correia da Silva <99354256+brunoHcSbr@users.noreply.github.com>
2023-08-29 21:36:38 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,220 @@
+var imagem40 = document.getElementById("img40");
+var texto40 = document.getElementById("texto40");
+
+var imagem50 = document.getElementById("img50");
+var texto50 = document.getElementById("texto50");
+
+var imagem60 = document.getElementById("img60");
+var texto60 = document.getElementById("texto60");
+
+var imagem70 = document.getElementById("img70");
+var texto70 = document.getElementById("texto70");
+
+var imagem80 = document.getElementById("img80");
+var texto80 = document.getElementById("texto80");
+
+var imagem90 = document.getElementById("img90");
+var texto90 = document.getElementById("texto90");
+
+var imagem2000 = document.getElementById("img2000");
+var texto2000 = document.getElementById("texto2000");
+
+imagem40.addEventListener("mouseover", function() {
+  //imagem.style.opacity = 0;
+  texto40.style.display = "block";
+  setTimeout(function() {
+    texto40.style.opacity = 1;
+  }, 10); // Pequeno atraso para permitir a transição
+});
+
+imagem40.addEventListener("mouseout", function() {
+  texto40.style.opacity = 0;
+  setTimeout(function() {
+    texto40.style.display = "none";
+    imagem40.style.opacity = 1;
+  }, 300); // Tempo da transição em milissegundos
+});
+
+const img40 = document.getElementById("img40");
+
+img40.addEventListener("mouseover", function() {
+    // Trocar a imagem ao passar o mouse
+    img40.src = "https://img.freepik.com/vetores-gratis/ilustracao-do-conceito-de-wireframing_114360-1407.jpg";
+});
+
+img40.addEventListener("mouseout", function() {
+  // Trocar a imagem ao passar o mouse
+  img40.src = "https://img.ibxk.com.br/2013/7/materias/203126194158.jpg?ims=328x";
+  }, 300); // Tempo da transição em milissegundos
+
+
+  imagem50.addEventListener("mouseover", function() {
+    texto50.style.display = "block";
+    setTimeout(function() {
+      texto50.style.opacity = 1;
+    }, 10); 
+  });
+  
+  imagem50.addEventListener("mouseout", function() {
+    texto50.style.opacity = 0;
+    setTimeout(function() {
+      texto50.style.display = "none";
+      imagem50.style.opacity = 1;
+    }, 300); // Tempo da transição em milissegundos
+  });
+  
+  const img50 = document.getElementById("img50");
+  
+  img50.addEventListener("mouseover", function() {
+      // Trocar a imagem ao passar o mouse
+      img50.src = "https://img.freepik.com/free-vector/web-design-production-maintenance-websites-web-graphic-interface-design-responsive-website-software-engineering-development-colorful-icon_335657-2699.jpg?w=2000";
+  });
+  
+  img50.addEventListener("mouseout", function() {
+    // Trocar a imagem ao passar o mouse
+    img50.src = "https://assets.sutori.com/user-uploads/image/d0a98406-08ad-4fa5-af12-37fb3c1fb779/aba11e790c2a43ac0e649c071f9c3dd4.gif";
+    }, 300); // Tempo da transição em milissegundos
+
+
+  imagem60.addEventListener("mouseover", function() {
+      //imagem.style.opacity = 0;
+      texto60.style.display = "block";
+      setTimeout(function() {
+        texto60.style.opacity = 1;
+      }, 10); // Pequeno atraso para permitir a transição
+  });
+    
+    imagem60.addEventListener("mouseout", function() {
+      texto60.style.opacity = 0;
+      setTimeout(function() {
+        texto60.style.display = "none";
+        imagem60.style.opacity = 1;
+      }, 300); // Tempo da transição em milissegundos
+    });
+    
+    const img60 = document.getElementById("img60");
+    
+    img60.addEventListener("mouseover", function() {
+        // Trocar a imagem ao passar o mouse
+        img60.src = "https://img.freepik.com/free-vector/web-design-production-maintenance-websites-web-graphic-interface-design-responsive-website-software-engineering-development-colorful-icon_335657-2699.jpg?w=2000";
+    });
+    
+    img60.addEventListener("mouseout", function() {
+      // Trocar a imagem ao passar o mouse
+      img60.src = "https://s2-techtudo.glbimg.com/SyuDfKJkxBO20NWX0gBU765j4Ro=/0x0:300x200/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/J/L/8f6gxiRhacwajBdA7wFQ/glbimg.com-po-tt-f-300x200-2011-08-18-ibm-logo.jpg";
+      }, 300); // Tempo da transição em milissegundos
+ 
+
+imagem70.addEventListener("mouseover", function() {
+  //imagem.style.opacity = 0;
+  texto70.style.display = "block";
+  setTimeout(function() {
+    texto70.style.opacity = 1;
+  }, 10); // Pequeno atraso para permitir a transição
+});
+
+imagem70.addEventListener("mouseout", function() {
+  texto70.style.opacity = 0;
+  setTimeout(function() {
+    texto70.style.display = "none";
+    imagem70.style.opacity = 1;
+  }, 300); // Tempo da transição em milissegundos
+});
+
+const img70 = document.getElementById("img70");
+
+img70.addEventListener("mouseover", function() {
+    // Trocar a imagem ao passar o mouse
+    img70.src = "https://img.freepik.com/vetores-gratis/ilustracao-do-conceito-de-wireframing_114360-1407.jpg";
+});
+
+img70.addEventListener("mouseout", function() {
+  // Trocar a imagem ao passar o mouse
+  img70.src = "https://i0.wp.com/boingboing.net/wp-content/uploads/2022/07/CPM.png?fit=560%2C384&ssl=1";
+  }, 300); // Tempo da transição em milissegundos
+
+  imagem80.addEventListener("mouseover", function() {
+    //imagem.style.opacity = 0;
+    texto80.style.display = "block";
+    setTimeout(function() {
+      texto80.style.opacity = 1;
+    }, 10); // Pequeno atraso para permitir a transição
+  });
+  
+  imagem80.addEventListener("mouseout", function() {
+    texto80.style.opacity = 0;
+    setTimeout(function() {
+      texto80.style.display = "none";
+      imagem80.style.opacity = 1;
+    }, 300); // Tempo da transição em milissegundos
+  });
+  
+  const img80 = document.getElementById("img80");
+  
+  img80.addEventListener("mouseover", function() {
+      // Trocar a imagem ao passar o mouse
+      img80.src = "https://img.freepik.com/vetores-gratis/ilustracao-do-conceito-de-wireframing_114360-1407.jpg";
+  });
+  
+  img80.addEventListener("mouseout", function() {
+    // Trocar a imagem ao passar o mouse
+    img80.src = "https://img.ibxk.com.br/2013/7/materias/203126194158.jpg?ims=328x";
+    }, 300); // Tempo da transição em milissegundos
+
+
+    imagem90.addEventListener("mouseover", function() {
+      //imagem.style.opacity = 0;
+      texto90.style.display = "block";
+      setTimeout(function() {
+        texto90.style.opacity = 1;
+      }, 10); // Pequeno atraso para permitir a transição
+  });
+    
+    imagem90.addEventListener("mouseout", function() {
+      texto90.style.opacity = 0;
+      setTimeout(function() {
+        texto90.style.display = "none";
+        imagem90.style.opacity = 1;
+      }, 300); // Tempo da transição em milissegundos
+    });
+    
+    const img90 = document.getElementById("img90");
+    
+    img90.addEventListener("mouseover", function() {
+        // Trocar a imagem ao passar o mouse
+        img90.src = "https://img.freepik.com/free-vector/web-design-production-maintenance-websites-web-graphic-interface-design-responsive-website-software-engineering-development-colorful-icon_335657-2699.jpg?w=2000";
+    });
+    
+    img90.addEventListener("mouseout", function() {
+      // Trocar a imagem ao passar o mouse
+      img90.src = "https://s2-techtudo.glbimg.com/hHTvJKX98j3N7y07kogFUUctOYY=/0x0:695x521/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/N/A/PBXcIkTPeHaViHQ5ZJ1Q/2017-02-23-compaq.png";
+      }, 300); // Tempo da transição em milissegundos
+ 
+
+      imagem2000.addEventListener("mouseover", function() {
+        //imagem.style.opacity = 0;
+        texto2000.style.display = "block";
+        setTimeout(function() {
+          texto2000.style.opacity = 1;
+        }, 10); // Pequeno atraso para permitir a transição
+    });
+      
+      imagem2000.addEventListener("mouseout", function() {
+        texto2000.style.opacity = 0;
+        setTimeout(function() {
+          texto2000.style.display = "none";
+          imagem2000.style.opacity = 1;
+        }, 300); // Tempo da transição em milissegundos
+      });
+      
+      const img2000 = document.getElementById("img2000");
+      
+      img2000.addEventListener("mouseover", function() {
+          // Trocar a imagem ao passar o mouse
+          img2000.src = "https://img.freepik.com/free-vector/web-design-production-maintenance-websites-web-graphic-interface-design-responsive-website-software-engineering-development-colorful-icon_335657-2699.jpg?w=2000";
+      });
+      
+      img2000.addEventListener("mouseout", function() {
+        // Trocar a imagem ao passar o mouse
+        img2000.src = "https://t2.tudocdn.net/595743?w=727&h=436";
+        }, 300); // Tempo da transição em milissegundos
